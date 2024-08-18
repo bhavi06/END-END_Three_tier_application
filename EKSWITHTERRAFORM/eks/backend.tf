ab04eac4a8db774aacadfb8d23359287ddcdcaf7
bhavi06 <58685725+bhavi06@users.noreply.github.com>
2024-08-18 13:14:11 +0530

    Update backend.tf

--- EKSWITHTERRAFORM/eks/backend.tf
+++ EKSWITHTERRAFORM/eks/backend.tf
@@ -6,13 +6,6 @@ terraform {
       version = "~> 5.49.0"
     }
   }
-  backend "s3" {
-    bucket         = "ekstatefile"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
-  }
 }
 
 provider "aws" {
